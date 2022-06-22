bcf52183692913785681a6eddf004852f2feca38
nathanth <nathan.hardenburg@icloud.com>
2022-06-22 15:56:19 -0500

    adds correct data to model and ejs

--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,23 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 	
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+    required: true
+  },
+  departs: {
+    type: Date
+  },
 }, {
   timestamps: true,
 })
