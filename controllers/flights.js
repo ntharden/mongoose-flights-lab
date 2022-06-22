c8f8597c196ee880b993196e72b3c374df812e45
nathanth <nathan.hardenburg@icloud.com>
2022-06-22 17:35:33 -0500

    updates and deletes flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,22 +4,24 @@ function newFlight(req, res) {
   res.render("flights/new", {
     title: "Add Flight"
   })
+  .catch(err => {
+    res.redirect('/flights/index')
+  })
 }
 
 function create(req, res) {
   Flight.create(req.body)
   .then(movie => {
-    res.redirect(`/flights`)
+    res.redirect(`/flights/index`)
   })
   .catch(err => {
-    res.redirect('/flights')
+    res.redirect('/flights/index')
   })
 }
 
 function index(req, res) {
   Flight.find({})
   .then(flights => {
-    console.log(flights)
     res.render("flights/index", {
       flights,
       title: "All Flights",
@@ -27,8 +29,61 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/show', {
+      title: 'Flight Detail',
+      flight
+    })
+  })
+  .catch(err => {
+    res.redirect('/')
+  })
+}
+
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(() => {
+    res.redirect("/flights")
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render("flights/edit", {
+      flight,
+      title: "Edit Flight"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+function update(req, res) {
+  Flight.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flights/${flight._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
 export {
   newFlight as new,
   create,
-  index
+  index,
+  show,
+  deleteFlight as delete,
+  edit,
+  update
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,13 +3,21 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+router.get('/index', flightsCtrl.index)
+
 // GET users listing
 router.get('/new', flightsCtrl.new)
 
 // POST localhost:3000/movies
 router.post('/', flightsCtrl.create)
 
-router.get('/index', flightsCtrl.index)
+router.get('/:id', flightsCtrl.show)
+
+router.delete('/:id', flightsCtrl.delete)
+
+router.get("/:id/edit", flightsCtrl.edit)
+
+router.put("/:id", flightsCtrl.update)
 
 export {
   router
